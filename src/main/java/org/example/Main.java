38780487d38ec9868631f875be0cb6c5db873bec
susanna.stepanian <sanastepanyan@gmail.com>
2023-10-05 22:16:32 +0300

    some bugs fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,35 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        in.close();
 
+        int[][] arr = new int[n][n];
         int counter = 1;
 
-        int [][] arr = new int[n][n];
-
-        for (int i = 0; i < n; i++){
-            if (n == 1) {
-                System.out.println(1);
-                break;
-            }
-            if (i % 2 == 0){
-                for (int j = 0; j < n ; j++) {
-                    arr[i][j] = counter;
-                    counter++;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = counter++;
                 }
             } else {
-                for (int j = n-1; j >= 0 ; j--) {
-                    arr[i][j] = counter;
-                    counter++;
+                for (int j = n - 1; j >= 0; j--) {
+                    arr[i][j] = counter++;
                 }
             }
         }
 
-        in.close();
+        int width = (int) Math.log10(n * n) + 1; // Визначення ширини поля для виводу
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j] + " ");
+                System.out.printf("%" + width + "d ", arr[i][j]);
             }
             System.out.println();
         }
-
     }
 }
